fd7c41d19af41729c7be87c308b3f4303704d234
thecodingsim <97773999+thecodingsim@users.noreply.github.com>
2022-02-18 14:17:28 -0800

    Create sorted_lists.py

---
+++ 3-working-with-lists/sorted_lists.py
@@ -0,0 +1,28 @@
+#SORTING LISTS IN NUMERICAL OR ALPHABETICAL ORDER, 
+# .sort(reverse=True) will sort list in a reverse order, descending
+
+# Checkpoint 1 & 2
+addresses = ["221 B Baker St.", "42 Wallaby Way", "12 Grimmauld Place", "742 Evergreen Terrace", "1600 Pennsylvania Ave", "10 Downing St."]
+# sort the addresses
+addresses.sort()
+print(addresses)
+#RESULT: ['10 Downing St.', '12 Grimmauld Place', '1600 Pennsylvania Ave', '221 B Baker St.', '42 Wallaby Way', '742 Evergreen Terrace']
+
+# Checkpoint 3
+names = ["Ron", "Hermione", "Harry", "Albus", "Sirius"]
+names.sort()
+print(names)
+
+
+# Checkpoint 4 & 5
+cities = ["London", "Paris", "Rome", "Los Angeles", "New York"]
+sorted_cities = cities.sort(reverse=True)
+print(sorted_cities)
+#The .sort() method does not return any value and thus does not need to be assigned to a variable. This is why we will see the value of None as our output for sorted_cities
+
+cities = ["London", "Paris", "Rome", "Los Angeles", "New York"]
+cities.sort(reverse=True)
+print(cities)
+
+#result: ['Rome', 'Paris', 'New York', 'Los Angeles', 'London']
+
