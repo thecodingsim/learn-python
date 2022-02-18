ae09e62459dcad7afc49abd804a3bf1c8f39ab79
thecodingsim <97773999+thecodingsim@users.noreply.github.com>
2022-02-18 13:42:34 -0800

    Create slicing_lists.py

---
+++ 3-working-with-lists/slicing_lists.py
@@ -0,0 +1,23 @@
+suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
+
+beginning = suitcase[0:4]
+
+
+# Your code below: 
+print(beginning)
+#we get ['shirt', 'shirt', 'pants', 'pants']
+
+#modifying beginning to select the first 2 elements of suitcase
+beginning = suitcase[0:2]
+#['shirt', 'shirt', 'pants', 'pants']
+
+#create a new list called "middle", it should contain the middle two items from suitcase. Print middle.
+
+middle = suitcase[2:4]
+print(middle)
+#['pants', 'pants']
+
+
+
+
+
