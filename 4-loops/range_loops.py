76b17d75ee59e67a8efe71f9e157f0cdd9b9c077
thecodingsim <97773999+thecodingsim@users.noreply.github.com>
2022-02-20 12:40:01 -0800

    Create range_loops.py

---
+++ 4-loops/range_loops.py
@@ -0,0 +1,18 @@
+# DAILY AFFIRMATIONS
+promise = "I will finish the python loops module!"
+# this will use range loops
+# Use the range() function in a "for" loop to print() out the provided promise variable five times.
+for temp in range(5):
+  print(promise)
+
+# exercise 2:
+
+goal = "I will finish python part 1 by the end of this month!"
+
+for temp in range(5):
+  print(goal)
+
+no_fear = "I will participate in hackathons and connect with people to learn and grow with them."
+
+for temp in range(5):
+  print(no_fear)
