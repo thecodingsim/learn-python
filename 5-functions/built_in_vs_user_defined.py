a60a0a28e040bbf7c8392e669656c41505a911da
thecodingsim <97773999+thecodingsim@users.noreply.github.com>
2022-02-20 23:26:49 -0800

    Update built_in_vs_user_defined.py

---
+++ 5-functions/built_in_vs_user_defined.py
@@ -0,0 +1,23 @@
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+# Write your code below:
+
+#use built-in function max() to find the max price
+max_price = max(9.75, 15.50, 5.99, 2.00)
+print(max_price)
+
+#find the min, use min()
+min_price = min(9.75, 15.50, 5.99, 2.00)
+print(min_price)
+
+# find the rounded price by 1 decimal place, use round() 
+rounded_price = round(tshirt_price, 1)
+print(rounded_price)
+
+# RESULTS: 
+# 15.5
+# 2.0
+# 9.8
