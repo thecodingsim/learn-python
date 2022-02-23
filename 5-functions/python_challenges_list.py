dd9c4bedb112760a50242851ac06924fb4d9081e
thecodingsim <97773999+thecodingsim@users.noreply.github.com>
2022-02-23 00:30:28 -0800

    Update python_challenges_list.py

--- 5-functions/python_challenges_list.py
+++ 5-functions/python_challenges_list.py
@@ -70,3 +70,85 @@ def combine_sort(lst1, lst2):
 
 
 print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+# --------------------------------------------------------------------------
+
+
+# ADVANCED LISTS
+
+#every three numbers:
+#Create a function called every_three_nums that has one parameter named start.
+
+#The function should return a list of every third number between start and 100 (inclusive). For example, every_three_nums(91) should return the list [91, 94, 97, 100]. If start is greater than 100, the function should return an empty list.
+
+def every_three_nums(start):
+  return list(range(start, 100, 3))
+
+print(every_three_nums(91))
+
+#---------------------------------------------------------------------------
+
+#remove middle
+#Create a function named remove_middle which has three parameters named lst, start, and end.
+
+#The function should return a list where all elements in lst with an index between start and end (inclusive) have been removed.
+
+#For example, the following code should return [4, 23, 42] because elements at indices 1, 2, and 3 have been removed: remove_middle([4, 8 , 15, 16, 23, 42], 1, 3)
+
+def remove_middle(lst, start, end):
+  return lst[:start] + lst[end+1:]
+print(remove_middle([4, 8, 15, 16, 23, 42], 1, 3))
+
+#------------------------------------------------------------------------
+
+# more frequent item
+#Create a function named more_frequent_item that has three parameters named lst, item1, and item2.
+#Return either item1 or item2 depending on which item appears more often in lst.
+#If the two items appear the same number of times, return item1.
+
+def more_frequent_item(lst, item1, item2):
+  if lst.count(item1) >= lst.count(item2):
+    return item1
+  else:
+    return item2
+
+print(more_frequent_item([2, 3, 3, 2, 3, 2, 3, 2, 3], 2, 3))
+
+#--------------------------------------------------------------------------
+
+#double index
+#Create a function named double_index that has two parameters: a list named lst and a single number named index.
+
+#The function should return a new list where all elements are the same as in lst except for the element at index. The element at index should be double the value of the element at index of the original lst.
+
+#If index is not a valid index, the function should return the original list.
+
+#For example, the following code should return [1,2,6,4] because the element at index 2 has been doubled: double_index([1, 2, 3, 4], 2)
+
+def double_index(lst, index):
+  if index >= len(lst):
+    return lst
+  else:
+    new_lst = lst[0:index]
+  new_lst.append(lst[index]*2)
+  new_lst = new_lst + lst[index+1:]
+  return new_lst
+
+print(double_index([3, 8, -10, 12], 2))
+
+#--------------------------------------------------------------------
+
+#middle item
+#Create a function called middle_element that has one parameter named lst.
+
+#If there are an odd number of elements in lst, the function should return the middle element. If there are an even number of elements, the function should return the average of the middle two elements.
+
+def middle_element(lst):
+  if len(lst) % 2 == 0:
+    sum = lst[int(len(lst)/2)] + lst[int(len(lst)/2) - 1]
+    return sum / 2
+  else:
+    return lst[int(len(lst)/2)]
+
+
+print(middle_element([5, 2, -10, -4, 4, 5]))
