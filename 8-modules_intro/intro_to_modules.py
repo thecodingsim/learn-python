4e12af23f12ef2bf51de61430a0780b5410d45d4
thecodingsim <97773999+thecodingsim@users.noreply.github.com>
2022-02-28 10:54:12 -0800

    update

--- 8-modules_intro/intro_to_modules.py
+++ 8-modules_intro/intro_to_modules.py
@@ -34,5 +34,57 @@ print(randomer_number)
 
 # namespace isolates the functions, classes, and variables defined in the module from the code in the file doing the importing. 
 
+# import module_name as name_you_pick_for_the_module
+# import* "*" is known as the wildcard and matches anything with everything. Becareful when using this because it can pollute the namespace
+# Pollution occurs when the same name could apply for two possible things.
+# random.sample() takes a range and a number as its arguments
+
+import codecademylib3_seaborn
+
+# Add your code below:
+from matplotlib import pyplot as plt
+
+import random
+numbers_a = range(1, 13)
+numbers_b = random.sample(range(1000), 12)
+#print(numbers_b)
+
+plt.plot(numbers_a, numbers_b)
+plt.show()
+
+# RESULT: image of plot 
+
+#------------------------
+
+# modules python decimals
+# use Decimal to fix floating points
 
 
+# Import Decimal below:
+from decimal import Decimal
+
+# Below are variables to return the amount of either two decimal points or four decimal points
+two_decimal_points = Decimal('0.2') + Decimal('0.69')
+print(two_decimal_points)
+
+four_decimal_points = Decimal('0.53') * Decimal('0.65')
+print(four_decimal_points)
+
+# result: 
+# 0.89
+# 0.3445
+
+#----------------------------
+
+#python files and scope
+# scope applies to classes and to the files you are working within. Files have scope.
+#files are modules, so you can give access to another file's content using import
+
+#in library.py
+def always_three():
+  return 3
+
+#results: 3
+#--------------------------------
+
+# https://www.youtube.com/watch?v=BVr-6Ki96XM&t=15s
